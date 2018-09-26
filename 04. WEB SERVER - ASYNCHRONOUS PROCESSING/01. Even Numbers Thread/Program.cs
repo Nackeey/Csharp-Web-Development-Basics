a7d7dff1802d7689d5c1814177283e31153106c9
Nackey <s2k@abv.bg>
2018-09-26 14:48:15 +0300

    some async examples

---
+++ 04. WEB SERVER - ASYNCHRONOUS PROCESSING/01. Even Numbers Thread/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _01._Even_Numbers_Thread
+{
+    using System;
+    using System.Threading;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Thread evens = new Thread(() => PrintEvenNumbers(1, 10000));
+            evens.Start();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine(line);
+
+                if (line == "exit")
+                {
+                    break; 
+                }
+
+            }
+
+            evens.Join();
+        }
+
+        private static void PrintEvenNumbers(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Thread.Sleep(1000);
+
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ 04. WEB SERVER - ASYNCHRONOUS PROCESSING/AsyncDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace AsyncDemo
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var number = Task.Run(() => NumberOfPrimesInInterval(2, 100000));
+            number.ContinueWith((task) => Console.WriteLine(task.Result));
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "exit")
+                {
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+
+        private static int NumberOfPrimesInInterval(int min, int max)
+        {
+            int count = 0;
+            for (int i = min; i < max; i++)
+            {
+                bool isPrime = true;
+                for (int j = 2;  j < i;  j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
