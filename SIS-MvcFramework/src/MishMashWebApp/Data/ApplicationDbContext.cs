43735e987c63e6e961944c9dc292d8dd6065661c
Nackey <s2k@abv.bg>
2018-10-24 14:52:44 +0300

    add app MishMash

---
+++ SIS-MvcFramework/src/MishMashWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿namespace MishMashWebApp.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MishMashWebApp.Models;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+       
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Channel> Channels { get; set; }
+        public DbSet<Tag> Tag { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-3OS7LSS\\SQLEXPRESS;Database=MishMash;Integrated Security=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ SIS-MvcFramework/src/MishMashWebApp/Startup.cs
@@ -0,0 +1,18 @@
+﻿namespace MishMashWebApp
+{
+    using SIS.MvcFramework;
+    using SIS.MvcFramework.Logger;
+    using SIS.MvcFramework.Services;
+
+    class Startup : IMvcApplication
+    {
+        public void Configure()
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection collection)
+        {
+            collection.AddService<ILogger, ConsoleLogger>();
+        }
+    }
+}
