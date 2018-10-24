14d2637f38e876b2eda0e14e2e6207cb85989286
Nackey <s2k@abv.bg>
2018-10-24 12:11:28 +0300

    SIS Web Server

--- WEB SERVER/SIS/SIS.HTTP/Extensions/StringExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SIS.HTTP.Extensions
-{
-    public static class StringExtensions
-    {
-        public static string Capitalize(this string word)
-            => word = char.ToUpper(word[0]) + word.Substring(1).ToLower();
-    }
-}
